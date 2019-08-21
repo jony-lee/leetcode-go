17d2d1dca4e3fb85d01b88aa802ed4e399967d27
junye zhang <zjy@rcrai.com>
2019-08-21 15:56:17 +0800

    update

--- 剑指offer/题23反转链表/main.go
+++ 剑指offer/题23反转链表/main.go
@@ -10,12 +10,12 @@ func reverseList(node *ListNode) *ListNode {
 	var pNode = node
 	var pPre *ListNode
 	for pNode != nil {
-		pNext := pNode.next
-		if pNext != nil { // 如果下一节点非空
-			PRH = pNode
+		pNext := pNode.next // 记录下一节点
+		if pNext != nil {   // 如果下一节点非空
+			PRH = pNode // 新反转链表头指向这个节点
 		}
-		pNode.next = pPre
-		pPre = pNode
+		pNode.next = pPre // 将本节点指向他的前一个节点,(第一个节点指向nil)
+		pPre = pNode      // 前一个节点更新为当前结点
 		pNode = pNext
 	}
 	return PRH
